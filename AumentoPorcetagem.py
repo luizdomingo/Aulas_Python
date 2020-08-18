bece37fd4326d0166e84c9120ae9e29d13ec4522
Luiz domingo da silva silva <62767505+luizdomingo@users.noreply.github.com>
2020-08-18 06:52:27 -0300

    testes

--- AumentoPorcetagem.py
+++ AumentoPorcetagem.py
@@ -2,7 +2,7 @@
 # Ex: 10% , retorne o valor do primeiro numero somado do almento do percentual do mesmo.
 
 def almento_percentual():
-    valor = float(input('Digite o valor do seu Sáçario: '))
+    valor = float(input('Digite o valor do seu Salario: '))
     percentual = float(input('Digite o quantos porcento de almento você Quer: '))
     resultado = valor + (valor * percentual / 100)
     print(resultado)
